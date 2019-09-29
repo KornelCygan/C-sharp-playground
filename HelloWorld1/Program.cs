d9c4ccc10bc4dbe86fe5b130400de65dc8e3195d
Korneliusz Cygan <KCygan@rockwellautomation.com>
2019-09-29 12:49:27 +0200

    Add project files.

---
+++ HelloWorld1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var age = 32;
+
+            //string firstName;
+
+            //firstName = Console.ReadLine();
+            //Console.Beep();
+            //Console.WriteLine("Hello " + firstName);
+
+            //if(firstName == "Mat")
+            //{
+            //    Console.WriteLine("Go away...");
+            //}
+            //else
+            //{
+            //    Console.WriteLine
+            //}
+            //Console.WriteLine($"You are {age} years old");
+
+            //Console.WriteLine("CALCULATOR");
+            //Console.WriteLine("First number to add");
+            //var first = Console.ReadLine();
+
+            //double firstNum = Convert.ToDouble(first);
+
+            //Console.WriteLine("Second number to add");
+            //var second = Console.ReadLine();
+
+            //double secondNum = Convert.ToDouble(second);
+
+            //var result = firstNum + secondNum;
+            //Console.WriteLine(result);
+
+            //Homework Calculator
+            Console.WriteLine("HomeWork Calculator");
+
+            Console.WriteLine("(A)dd");
+            Console.WriteLine("(S)ubstract");
+            Console.WriteLine("(D)evide");
+            Console.WriteLine("(M)ultiply");
+            Console.WriteLine("What You want to do?");
+            string operation = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Type in first number");
+            var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Type in second number");
+            var secondNumber = Convert.ToDouble(Console.ReadLine());
+            double result;
+
+            switch (operation)
+            {
+                case "A":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "S":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "D":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "M":
+                    result = firstNumber * secondNumber;
+                    break;
+                default:
+                    result = 0;
+                    Console.WriteLine("Operation is unknown.");
+                    break;
+            }
+            
+            Console.WriteLine($"Result: { result }");
+
+
+        }
+    }
+}
---
+++ lesson 2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace lesson_2
+{
+    class Program
+    {
+        static void WelcomeUser()
+        {
+            Console.WriteLine("What`s your name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+        }
+
+        static int RequestNumber(string numberName)
+        {
+            Console.WriteLine($"What number do you want to use as {numberName} ");
+
+            int exponentBase = int.Parse(Console.ReadLine());
+
+            return exponentBase;
+        }
+
+        static int Exponent (int exponentBase, int exponent)
+        {
+            int result = 1;
+            for(int i = 0; i < exponent; i++)
+            {
+                result = exponentBase * result;
+            }
+
+            return result;
+        }
+        static int RequestExponentBase()
+        {
+            Console.WriteLine("What number do you want exponentate");
+
+            int exponentBase = int.Parse(Console.ReadLine());
+
+            return exponentBase;
+        }
+
+        static int exponentOf2(int number)
+        {
+            return number * number;
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine($"Result: {result} ");
+        }
+
+        static bool DecideOnRunningProgramAgain()
+        {
+            Console.WriteLine("Try Again? Y/N");
+            var userResult = Console.ReadLine();
+
+            if (userResult == "Y" || userResult == "y")
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool runProgramAgain;
+
+            do
+            {
+                WelcomeUser();
+                int exponentBase = RequestExponentBase();
+                int result = exponentOf2(exponentBase);
+                PrintResult(result);
+                runProgramAgain = DecideOnRunningProgramAgain();
+
+            } while (runProgramAgain);
+
+            //while(runProgramAgain)
+            //{
+            //    WelcomeUser();
+            //    int exponentBase = RequestExponentBase();
+            //    int result = exponentOf2(exponentBase);
+            //    PrintResult(result);
+            //    runProgramAgain = DecideOnRunningProgramAgain();
+            //}
+
+            Console.WriteLine("Bye bye");
+            Console.ReadLine();
+            
+
+            
+        }
+    }
+}
+;
